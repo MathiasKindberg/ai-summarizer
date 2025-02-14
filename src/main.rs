ab992e13ae28e126b2d6a1189d5e6dfd79d93fad
Mathias Kindberg <mathias@kindberg.earth>
2025-02-14 13:54:02 +0100

    Add file logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use tracing_subscriber::util::SubscriberInitExt;
+
 pub(crate) mod config;
 pub(crate) mod db;
 pub(crate) mod google_chat;
@@ -213,18 +215,32 @@ async fn get_summary(args: Args) -> anyhow::Result<()> {
 
 #[tokio::main]
 async fn main() {
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(tracing::Level::INFO)
-        // completes the builder.
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    use tracing_subscriber::layer::Layer;
+    use tracing_subscriber::layer::SubscriberExt;
+
+    let file_appender = tracing_appender::rolling::daily("./log", "ai_summarizer.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let file_layer = tracing_subscriber::fmt::layer();
+    let file_layer = file_layer
+        .with_writer(non_blocking)
+        .json()
+        .with_filter(tracing::level_filters::LevelFilter::INFO)
+        .boxed();
+
+    let pretty_layer = tracing_subscriber::fmt::layer()
+        .pretty()
+        .with_writer(std::io::stdout)
+        .with_filter(tracing::level_filters::LevelFilter::INFO)
+        .boxed();
+
+    tracing_subscriber::registry()
+        .with(file_layer)
+        .with(pretty_layer)
+        .init();
 
     use clap::Parser;
     let args = Args::parse();
 
     get_summary(args).await.expect("Failed to get summary");
-
-    tracing::info!("Exiting");
 }
